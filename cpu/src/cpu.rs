ef50802fa92ad8238fb919ca5399c495a7b1947a
Sai Yashwanth <taddishetty34@gmail.com>
2024-03-05 06:52:09 +0530

    Add: Method to initialize CPU

--- cpu/src/cpu.rs
+++ cpu/src/cpu.rs
@@ -4,27 +4,35 @@
 //      resetPC
 //      outerLoop - source addr, dest addr, next instruction addr. Update pc to next inst addr.
 
+// 16 MiB
+const MEM_SIZE: usize = 0x1000008;
+
 struct CPU{
-    mem,
-    pc 
+    mem: [u8; MEM_SIZE],
+    pc: u32, 
 }
 
 impl CPU {
-    // 
+    // Implement CPU, all its methods 
    fn new(arg: Type) -> CPU {
-       
+       self{
+
+           mem = [0; MEM_SIZE];
+           // pc = mem + (mem[2]<<16 | mem[3]<<8 | mem[4]); given in docs
+           pc = mem + (mem[2]<<16 | mem[3]<<8 | mem[4]);  
+       }
    }
    fn loadRom(arg: Type) -> RetType {
 
    }
    fn read(arg: Type) -> RetType {
-       unimplemented!();
+       
    }
    fn write(arg: Type) -> RetType {
-       unimplemented!();
+      
    }
    fn innerLoop(arg: Type) -> RetType {
-       unimplemented!();
+      
    }
 }
 
